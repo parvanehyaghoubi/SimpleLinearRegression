b6919b43c63615bb4edd9d54d1637b5236f85b4e
P-Yaghoubi <146029871+P-Yaghoubi@users.noreply.github.com>
2024-05-20 03:27:51 +0330

    Add docStrings

--- SimpleLinearRegression.py
+++ SimpleLinearRegression.py
@@ -1,7 +1,48 @@
 import numpy as np
 
 def find_slope_intercept(xi , yi):
-    
+    """
+    Compute the slope and intercept value in a simple linear equation for xi and yi data. 
+
+    Parameters
+    ------
+    xi (array_like): The 1D array of data for which to calculate the slope and intercept. 
+    yi (array_like): The 1D array of data for which to calculate the slope and intercept. 
+
+    Returns
+    ------
+    slope, intercept: Return the slope and intercept of a simple linear equation. 
+
+    Raises 
+    ------
+    ValueError: If the number of xi and yi elements are not equal. 
+    ZeroDivisionError: If the denominator of the slope is zero. 
+
+    Examples
+    ------
+    >>> xi = np.array([1, 2, 3])
+    >>> yi = np.array([4, 5, 6])
+    >>> find_slope_intercept(xi , yi)
+    The slope is: 1.0 and the intercept is: 3.0. 
+    Your simple linear equation is y = 1.0 x + 3.0
+
+    ------
+
+    >>> xi = np.array([1])
+    >>> yi = np.array([2])
+    >>> find_slope_intercept(xi , yi)
+    Traceback (most recent call last):
+        raise ZeroDivisionError("The denominator of the slope can not be zero.")
+
+    ------
+
+    >>> xi = np.array([1, 2])
+    >>> yi = np.array([4, 5, 6])
+    >>> find_slope_intercept(xi , yi)
+    Traceback (most recent call last):
+        raise ValueError("The number of xi and yi elements must be equal.")
+
+    """
     if len(xi) != len(yi):
         raise ValueError("The number of xi and yi elements must be equal.")
     
